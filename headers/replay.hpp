5f5bedd2a973e9b17c334079b4875896079584ff
Ismail Choukri <ichoukri@e2r5p4.1337.ma>
2022-11-21 14:33:09 +0100

    modify some replay

--- headers/replay.hpp
+++ headers/replay.hpp
@@ -2,18 +2,18 @@
 #define REPLAY_HPP
 
 #define CLIENT_INFORMATION _clientMap[fd]->get_Nickname() + "!" + _clientMap[fd]->get_Username() + "@" + _clientMap[fd]->get_Host()
-#define ERR_NEEDMOREPARAMS " : Not enough parameters"
-#define ERR_ALREADYREGISTERED  " :You may not reregister"
-#define ERR_PASSWDMISMATCH " :Password incorrect"
-#define ERR_NONICKNAMEGIVEN " :No nickname given"
-#define ERR_NICKNAMEINUSE " :Nickname is already in use"
-#define ERR_ERRONEUSNICKNAME  " :Erroneus nickname"
+#define ERR_NEEDMOREPARAMS ":Not enough parameters"
+#define ERR_ALREADYREGISTERED  ":You may not reregister"
+#define ERR_PASSWDMISMATCH ":Password incorrect"
+#define ERR_NONICKNAMEGIVEN ":No nickname given"
+#define ERR_NICKNAMEINUSE ":Nickname is already in use"
+#define ERR_ERRONEUSNICKNAME  ":Erroneus nickname"
 #define RPL_WELCOME " :Welcome to the " + _name + " Network, " + CLIENT_INFORMATION
 #define ERR_NOTREGISTERED ":You have not registered"
-#define ERR_NOSUCHCHANNEL " No such channel"
-#define ERR_BADCHANNELKEY names[i] + " :Cannot join channel (+k)\n"
-#define ERR_NOTONCHANNEL  " :You're not on that channel\n"
-#define RPL_NOTOPIC  " :No topic is set\n"
+#define ERR_NOSUCHCHANNEL ":No such channel"
+#define ERR_BADCHANNELKEY ":Cannot join channel (+k)"
+#define ERR_NOTONCHANNEL  ":You're not on that channel"
+#define RPL_NOTOPIC  ":No topic is set"
 #define ERR_CHANOPRIVSNEEDED  ":You're not channel operator"
 #define ERR_NOSUCHNICK  ":No such nick/channel"
 #define ERR_KEYSET  ":Channel key already set"
